bd87eb49c425d49a13184331cda5be4632a7793e
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-19 23:02:05 +0200

    [ADD] setting initialization

---
+++ sources/Compute.py
@@ -0,0 +1,44 @@
+#  B4 - COMPUTER NUMERICAL ANALYSIS
+#      -----------------------
+#           TRADE PROJECT
+#
+# Repository:
+# https://github.com/sheiiva/CNA_trade
+#
+# (05/19/2020)
+# Authors:  Corentin COUTRET-ROZET  <corentin.rozet@epitech.eu>
+#           Patricia Monfa-Matas    <patricia.monfa-matas@epitech.eu>
+#
+
+
+class Compute():
+
+    """
+    Compute class. Provide computing tools or specific trading computaitons.
+    """
+
+    def isInt(self, value):
+
+        """
+        Return True if 'value' is an int. Return False otherwise. 
+        """
+
+        try:
+            int(value)
+        except ValueError:
+            return False
+        else:
+            return True
+
+    def isFloat(self, value):
+
+        """
+        Return True if 'value' is a float. Return False otherwise. 
+        """
+
+        try:
+            float(value)
+        except ValueError:
+            return False
+        else:
+            return True
--- sources/Trade.py
+++ sources/Trade.py
@@ -11,6 +11,30 @@
 #
 
 from sources.Parser import Parser
+from sources.Compute import Compute
+
+## DEFINE CANDLE FORMAT
+CANDLE_FORMAT  = {
+    # pair:   The chart to which candle belongs.
+    "PAIR":     0,
+    # date:   Unix timestamp, which represents a certain date and time.
+    "DATE":     1,
+    # high:   The highest price traded in this candle.
+    "HIGH":     2,
+    # low:    The lowest price traded in this candle.
+    "LOW" :     3,
+    # open:   The opening price of this candle.
+    "OPEN":     4,
+    # close:  The closing price of this candle.
+    "CLOSE":    5,
+    # volume: The total volume that has been traded in this candle.
+    "VOLUME":   6
+}
+
+# DEFINE INPUT FORMAT
+COMMAND   = 0
+VARIABLE  = 1
+VALUE     = 2
 
 class Trade():
 
@@ -18,14 +42,17 @@ class Trade():
     Main class of the Trade project.
     """
 
-
     def __init__(self):
 
         """
         Initialization of Trade Class's attributes.
         """
         self._state = True
+        self._settings = {}
+
+        # Attributes to other classes
         self._parser = Parser()
+        self._compute = Compute()
 
         # Run program's main loop
         self.run()
@@ -40,10 +67,31 @@ class Trade():
             inputs = input()
         except EOFError:
             exit(84)
-        except InterruptedError:
-            exit(84)
+        except KeyboardInterrupt:
+            print("\nexit.")
+            exit(0)
         else:
             return inputs
+
+    def initSettings(self, inputs) -> None:
+
+        """
+        Parse the input passed as argument
+        and initialise corresponding class's attribute.
+        """
+
+        if inputs[VARIABLE] is "candle_format":
+            # Candle format is set by default, don't need to stock it.
+            return
+
+        if self._compute.isInt(inputs[VALUE]):
+            value = int(inputs[VALUE])
+        elif self._compute.isFloat(inputs[VALUE]):
+            value = float(inputs[VALUE])
+        else:
+            value = inputs[VALUE]
+
+        self._settings[inputs[VARIABLE]] = value
 
     def run(self) -> None:
 
@@ -54,3 +102,8 @@ class Trade():
         while (self._state):
             strInput = self.getInput()
             inputs = self._parser.toList(strInput)
+            if len(inputs) == 0:
+                continue
+            if inputs[0] == "settings":
+                self.initSettings(inputs)
+            print(self._settings)
